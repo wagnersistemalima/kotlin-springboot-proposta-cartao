d5fb4390794f39824f68c0622f28d2b8f3d8237d
Wagner de Lima Braga Silva <wagner.sistemalima@gmail.com>
2021-07-17 10:51:53 -0300

    Melhorando a visibilidade da nossa aplicação para equipe de operação Health check

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
